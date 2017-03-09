0de10d22844b88e56dfdc52a193636b5909f78d6
Ricky <rickycxj@gmail.com>
2017-03-09 18:17:58 +0800

    rubocop检查

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,18 +4,14 @@ ActiveAdmin.register User do
   # 用户认证审核通过
   member_action :user_audit, method: :post do
     user_extra = resource.user_extra
-    if user_extra.blank?
-      return render 'user_audit_failed'
-    end
+    return render 'user_audit_failed' if user_extra.blank?
     user_extra.passed!
   end
 
   # 用户审核不通过
   member_action :user_audit_forbid, method: :post do
     user_extra = resource.user_extra
-    if user_extra.blank?
-      return render 'user_audit_failed'
-    end
+    return render 'user_audit_failed' if user_extra.blank?
     user_extra.failed!
   end
 end

f30dfaf8577f9720be435b881bf6fb858bf84934
breeze-yang <breeze@deshpro.com>
2017-03-14 10:34:27 +0800

    完成race的测试用例

--- spec/factories/races.rb
+++ spec/factories/races.rb
@@ -4,10 +4,9 @@ FactoryGirl.define do
     logo File.open(Rails.root.join('spec/factories/foo.png'))
     sequence(:prize) { |n| "100_000_#{n}" }
     sequence(:location) { |n| "澳门_#{n}" }
-    sequence(:seq_id) { |n| n }
     ticket_status 'unsold'
     ticket_price 10_000
-    sequence(:begin_date) { Random.rand(1..9).days.since.strftime('%Y-%m-%d') }
-    sequence(:end_date)   { Random.rand(11..19).days.since.strftime('%Y-%m-%d') }
+    sequence(:begin_date) { Random.rand(1..9).days.since.to_date }
+    sequence(:end_date)   { Random.rand(11..19).days.since.to_date }
   end
 end

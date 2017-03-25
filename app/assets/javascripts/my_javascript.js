9f82667766397b9bdcc6b54988ebee01df5964a5
Ricky <rickycxj@gmail.com>
2017-03-25 11:43:17 +0800

    订单view重构

--- app/assets/javascripts/my_javascript.js
+++ app/assets/javascripts/my_javascript.js
@@ -1,12 +1,12 @@
 $(function(){
     $(".order_price_modified").bind('click', function(){
-        $(".change_order_price").show();
+        $(".change_price").show();
         $(".order_price").hide();
         $(".order_price_modified").hide();
     })
 
     $(".order_button_cancel").bind('click', function(){
-        $(".change_order_price").hide();
+        $(".change_price").hide();
         $(".order_price").show();
         $(".order_price_modified").show();
         return false;

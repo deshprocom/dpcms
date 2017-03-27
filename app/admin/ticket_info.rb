2e08dd4d07bf3228004d50f85e9770ad89dd66de
breeze-yang <breeze@deshpro.com>
2017-03-27 10:32:00 +0800

    票务管理中增加剩票提示

--- app/admin/ticket_info.rb
+++ app/admin/ticket_info.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register TicketInfo do
     if result.failure?
       flash[:error] = result.msg
     else
-      flash[:success] = result.msg
+      flash[:success] = '修改成功，请将售票状态更改回售票中'
     end
     redirect_to admin_race_path(resource.race, anchor: 'ticket_manage')
   end

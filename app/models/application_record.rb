e7c8e9c550f5bab2d7f117719a173e16ac54401f
breeze-yang <breeze@deshpro.com>
2017-03-28 18:41:59 +0800

    删除app/models

--- app/models/application_record.rb
+++
@@ -1,4 +0,0 @@
-# 引入ActiveRecord处理模型相关的工作
-class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-end
--- app/models/user_validator.rb
+++
@@ -1,7 +0,0 @@
-# 用户相关的验证器
-class UserValidator
-  include UserUniqueValidator
-  include EmailValidator
-  include MobileValidator
-  include PasswordValidator
-end

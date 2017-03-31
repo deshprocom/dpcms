3eca6b7a185bdcad007c46b2541e42f044335718
breeze-yang <breeze@deshpro.com>
2017-03-31 10:24:35 +0800

    更新DP_DATABASE_TEST

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 ENV['RAILS_ENV'] = 'test'
-ENV['DP_DATABASE_TEST'] = 'deshpro_s_i_t'
+ENV['DP_DATABASE_TEST'] ||= 'deshpro_s_i_t'
 require 'cucumber/rails'
 require 'capybara/poltergeist'
 

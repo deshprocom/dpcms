136f9d22af56cb580d67210f275ecc45146c54e0
breeze-yang <breeze@deshpro.com>
2017-04-12 18:52:49 +0800

    完成通过dialog 增加边赛排行榜

--- app/admin/race_rank.rb
+++ app/admin/race_rank.rb
@@ -7,4 +7,36 @@ ActiveAdmin.register RaceRank do
   menu false
 
   permit_params :ranking, :earning, :score, :player_id, :race_id
+  index title: proc { "#{@race.name} - 排行榜" }, download_links: false do
+    render 'index', context: self
+  end
+
+  controller do
+    before_action :set_race, only: [:new, :create]
+    def new
+      @race_rank = @race.race_ranks.build
+    end
+
+    def create
+      @race_rank = @race.race_ranks.build(permitted_params[:race_rank])
+      respond_to do |format|
+        if @race_rank.save
+          format.html { redirect_to admin_race_race_ranks_path(@race), notice: 'rank新增成功。' }
+          format.js
+        else
+          format.html { render :new }
+          format.js
+        end
+      end
+    end
+
+    def set_race
+      @race = Race.find(params[:race_id])
+    end
+  end
+
+  config.clear_action_items!
+  action_item :add, only: :index do
+    link_to '新增排名', new_admin_race_race_rank_path(race), remote: true
+  end
 end
--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -1,6 +1,2 @@
 #= require active_admin/base
 #= require_tree ./admin
-#$('body.page_races_show').ready( ->
-# #setInterval(shake_ticket_number, 1000);
-#  alert(3)
-#);

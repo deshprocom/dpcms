953359f852254a717a577e37550abf4b0ce29026
Ricky <rickycxj@gmail.com>
2017-04-21 12:16:40 +0800

    增加资讯列表

--- app/admin/info.rb
+++ app/admin/info.rb
@@ -1,6 +1,10 @@
 ActiveAdmin.register Info do
   menu label: '资讯管理', priority: 5
 
+  filter :title
+  filter :date
+  filter :published
+
   index title: '资讯列表' do
     column '资讯图片', :image do |info|
       link_to image_tag('http://localhost/example', height: 60)
@@ -17,5 +21,9 @@ ActiveAdmin.register Info do
     column :top
     column :published
     actions name: '操作'
+  end
+
+  sidebar :'资讯数目', only: :index do
+    "资讯共：#{Info.count}条"
   end
 end

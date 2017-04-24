4ed3ab9b5cbd4d7fbfb9da46a1badc5488f8be70
Ricky <rickycxj@gmail.com>
2017-04-24 14:43:18 +0800

    修改info

--- app/admin/info.rb
+++ app/admin/info.rb
@@ -24,7 +24,42 @@ ActiveAdmin.register Info do
     column :info_type_id
     column :top
     column :published
-    actions name: '操作'
+    actions name: '操作', class: 'info_actions' do |type|
+      if type.published
+        item '取消发布', unpublish_admin_info_path(type),
+             data: { confirm: '确定取消吗？' }, method: :post
+      else
+        item '发布', publish_admin_info_path(type),
+             data: { confirm: '确定发布吗？' }, method: :post
+      end
+      if type.top
+        item '取消置顶', untop_admin_info_path(type),
+             data: { confirm: '确定取消置顶吗？' }, method: :post
+      else
+        item '置顶', top_admin_info_path(type),
+             data: { confirm: '确定置顶吗？' }, method: :post
+      end
+    end
+  end
+
+  member_action :publish, method: :post do
+    resource.publish!
+    redirect_back fallback_location: admin_infos_url, notice: '发布成功'
+  end
+
+  member_action :unpublish, method: :post do
+    resource.unpublish!
+    redirect_back fallback_location: admin_infos_url, notice: '取消发布成功'
+  end
+
+  member_action :top, method: :post do
+    resource.top!
+    redirect_back fallback_location: admin_infos_url, notice: '发布成功'
+  end
+
+  member_action :untop, method: :post do
+    resource.untop!
+    redirect_back fallback_location: admin_infos_url, notice: '取消发布成功'
   end
 
   sidebar :'资讯数目', only: :index do

232efee689c4a664f0ef38ca2a881e212f6abe26
breeze-yang <breeze@deshpro.com>
2017-04-28 17:53:31 +0800

    make editor support upload image

---
+++ app/uploaders/race_photo_uploader.rb
@@ -0,0 +1,10 @@
+class RacePhotoUploader < BaseUploader
+
+  version :preview do
+    process resize_to_fit: [200, 200]
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end

7497a459298701b7b682fc67b383f773cd2352d4
breeze-yang <breeze@deshpro.com>
2017-05-05 14:13:16 +0800

    完善race_photo_uploader.rb

--- app/uploaders/race_photo_uploader.rb
+++ app/uploaders/race_photo_uploader.rb
@@ -8,15 +8,15 @@ class RacePhotoUploader < BaseUploader
   def filename
     return if super.nil?
 
-    @name ||= Digest::MD5.hexdigest(super)
+    @name ||= Digest::MD5.hexdigest(current_path)
     "#{@name}.#{file.extension.downcase}"
   end
 
   ALLOW_VERSION_MAPPING = {
     sm: 'iopcmd=thumbnail&type=4&width=200',
     md: 'iopcmd=thumbnail&type=4&width=500',
-    lg: 'iopcmd=thumbnail&type=4&width=900',
-  }
+    lg: 'iopcmd=thumbnail&type=4&width=900'
+  }.freeze
   def url(version_name = nil)
     @url ||= super({})
     return @url if version_name.blank?

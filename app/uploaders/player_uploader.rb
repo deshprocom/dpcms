02127305b7361581c7b3c3627ebade2ce3b10e84
Ricky <rickycxj@gmail.com>
2017-08-14 15:50:53 +0800

    牌手图像名字在上传的时候统一英文

--- app/uploaders/player_uploader.rb
+++ app/uploaders/player_uploader.rb
@@ -16,6 +16,10 @@ class PlayerUploader < BaseUploader
     %w(jpg jpeg gif png)
   end
 
+  def filename
+    "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
   # rubocop:disable Style/GuardClause
   def crop
     if model.crop_x.present?
@@ -30,5 +34,11 @@ class PlayerUploader < BaseUploader
         img
       end
     end
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
   end
 end

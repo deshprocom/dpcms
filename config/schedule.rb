4f52ff10135de86defa5b4c793ca9348165185ed
Ricky <rickycxj@gmail.com>
2017-11-21 19:06:18 +0800

    每分钟检查一次

--- config/schedule.rb
+++ config/schedule.rb
@@ -14,6 +14,6 @@ every :hour, at: '2:30am' do
   rake 'batch_order:complete_delivered_15_days'
 end
 
-every :min do
+every 1.minute do
   rake 'batch_order: cancel_product_unpaid_order_half_an_hour'
 end

c61f10d97b5215bfdd07e6d65bbc74fdf028958c
breeze-yang <breeze@deshpro.com>
2017-12-06 10:58:52 +0800

    修改wx_pay\

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -1,4 +1,7 @@
 # required
+# 测试环境时不初始WxPay
+return if Rails.env.test?
+
 WxPay.appid = ENV['WX_APP_ID']
 WxPay.key = ENV['WX_APP_KEY']
 WxPay.mch_id = ENV['WX_MCH_ID']
@@ -10,6 +13,3 @@ WxPay.apiclient_cert = File.read(ENV['WX_APICLIENT_CERT'])
 
 # optional - configurations for RestClient timeout, etc.
 WxPay.extra_rest_client_options = { timeout: 30, open_timeout: 30 }
-
-# 测试环境时，调用微信的沙盒环境
-WxPay.sandbox_mode = true if Rails.env.test?

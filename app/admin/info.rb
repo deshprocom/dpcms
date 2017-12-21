dab67c1cf4563438d7bd9d796ba4cd24fca1579e
Ricky <rickycxj@gmail.com>
2017-12-21 15:12:14 +0800

    修复rubocop

--- app/admin/info.rb
+++ app/admin/info.rb
@@ -40,15 +40,11 @@ ActiveAdmin.register Info do
     column :en_is_show do |info|
       info.info_en&.is_show ? '√' : '×'
     end
-    column :topic_likes do |info|
-      info.total_likes
-    end
+    column :topic_likes, &:total_likes
     column :topic_comments do |info|
       info.comments.count
     end
-    column :total_views do |info|
-      info.total_views
-    end
+    column :total_views, &:total_views
     column :top
     column :published
     actions name: '操作', class: 'info_actions' do |resource|

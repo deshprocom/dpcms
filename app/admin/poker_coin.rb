c9e3874f1772fe6df2a134e664e340f247fc3573
Ricky <rickycxj@gmail.com>
2018-01-26 18:33:12 +0800

    扑客币

--- app/admin/poker_coin.rb
+++ app/admin/poker_coin.rb
@@ -15,10 +15,12 @@ ActiveAdmin.register PokerCoin do
   end
 
   member_action :coin, method: [:get, :post] do
+    @max_coin = PokerCoin.total_coin_of_the_type(resource.typeable, resource.user)
     return render 'coin' unless request.post?
-    return render 'params_blank' if params[:number].to_i.zero? || params[:memo].blank?
-    return render 'number_format_error' unless params[:number].to_i.is_a?(Numeric)
-    # 添加记录
+    return render 'params_blank' if params[:number].to_f.zero? || params[:memo].blank?
+    return render 'number_format_error' unless params[:number].to_f.is_a?(Numeric)
+    return render 'over_number' if @max_coin < (-params[:number].to_f)
+        # 添加记录
     PokerCoin.create(user: resource.user, typeable: resource.typeable, memo: params[:memo], number: params[:number])
     render 'common/update_success'
   end

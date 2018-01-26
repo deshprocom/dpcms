33d2e558f36143c6338193ed74d8189009c4636a
Ricky <rickycxj@gmail.com>
2018-01-26 15:43:16 +0800

    修改poker

--- app/admin/poker_coin.rb
+++ app/admin/poker_coin.rb
@@ -2,6 +2,14 @@ ActiveAdmin.register PokerCoin do
   menu priority: 25
   config.clear_action_items!
 
+  filter :typeable_type
+  filter :typeable_id
+  filter :user_nick_name, as: :string
+  filter :user_mobile, as: :string
+  filter :memo
+  filter :number
+  filter :created_at
+
   index do
     render 'index', context: self
   end

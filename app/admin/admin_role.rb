079d5b7ebd009deb7795453d3e124d7f9a89f50b
breeze-yang <breeze@deshpro.com>
2018-03-14 17:40:37 +0800

    增加权限管理

---
+++ app/admin/admin_role.rb
@@ -0,0 +1,37 @@
+ActiveAdmin.register AdminRole do
+  config.filters = false
+  menu priority: 1, parent: '用户管理'
+  permit_params :name, permissions: []
+  form do |f|
+    f.inputs do
+      f.input :name, label: '角色名称'
+      f.input :permissions,
+              as: :check_boxes,
+              collection: CmsAuthorization.permissions_with_trans
+    end
+    f.actions
+  end
+
+  index do
+    id_column
+    column(:name)
+    column(:permissions) { |role| role.permissions_text }
+    actions
+  end
+
+  show do
+    attributes_table do
+      row(:id)
+      row(:name)
+      row(:permissions) { |role| role.permissions_text }
+    end
+  end
+
+  controller do
+    before_action :process_params, only: [:create, :update]
+
+    def process_params
+      params[:admin_role][:permissions].reject!(&:empty?)
+    end
+  end
+end

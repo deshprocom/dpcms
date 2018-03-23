262bf5f419f2f6c3bd0d93827f158d44facb5a5d
breeze-yang <breeze@deshpro.com>
2018-03-23 14:30:49 +0800

    将filename md5化 放到公用的base中 (#682)
    * 将filename md5化 放到公用的base中
    
    * 根据rubocop修改代码

--- app/uploaders/category_uploader.rb
+++ app/uploaders/category_uploader.rb
@@ -1,13 +1,6 @@
 class CategoryUploader < BaseUploader
   process resize_to_limit: [512, nil]
 
-  def filename
-    return if super.nil?
-
-    @md5_name ||= Digest::MD5.hexdigest(current_path)
-    "#{@md5_name}.#{file.extension.downcase}"
-  end
-
   # 在 UpYun 配置图片缩略图
   # http://docs.upyun.com/guide/#_12
   # 固定宽度,高度自适应

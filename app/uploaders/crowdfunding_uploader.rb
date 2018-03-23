9fbffb75607e36341e8cc9ad58d79aa73406f39d
breeze-yang <breeze@deshpro.com>
2018-03-23 14:31:39 +0800

    将filename md5化 放到公用的base中 (#682) (#683) (#684)
    * 将filename md5化 放到公用的base中
    
    * 根据rubocop修改代码

--- app/uploaders/crowdfunding_uploader.rb
+++ app/uploaders/crowdfunding_uploader.rb
@@ -3,13 +3,6 @@ class CrowdfundingUploader < BaseUploader
     "uploads/crowdfunding/#{mounted_as}/#{model.id}"
   end
 
-  def filename
-    return if super.nil?
-
-    @md5_name ||= Digest::MD5.hexdigest(current_path)
-    "#{@md5_name}.#{file.extension.downcase}"
-  end
-
   ALLOW_VERSIONS = %w(xs sm md lg).freeze
   def url(version_name = nil)
     @url ||= super({})
